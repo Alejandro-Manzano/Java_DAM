506d75cd881dde07b4fe19d188f55a7bc3797f06
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-11-29 10:59:26 +0100

    new Perro

--- Clase_2/src/ejercicios_Practicos/Perro.java
+++ Clase_2/src/ejercicios_Practicos/Perro.java
@@ -2,4 +2,45 @@ package ejercicios_Practicos;
 
 public class Perro {
 
+private int patas;
+private String nombre;
+private int edad;
+
+public int getPatas() {
+return patas;
+}
+
+public void setPatas(int patas) {
+this.patas = patas;
+}
+
+public String getNombre() {
+return nombre;
+}
+
+public void setNombre(String nombre) {
+this.nombre = nombre;
+}
+
+public int getEdad() {
+return edad;
+}
+
+public void setEdad(int edad) {
+this.edad = edad;
+}
+
+
+public Perro(String nombre, int patas, int edad) {
+setNombre(nombre);
+setPatas(patas);
+setEdad(edad);
+
+}
+
+public void ladrar() {
+System.out.println("guau guau");
+}
+
+
 }
--- Clase_2/src/ejercicios_Practicos/Principal.java
+++ Clase_2/src/ejercicios_Practicos/Principal.java
@@ -1,10 +1,30 @@
+
 package ejercicios_Practicos;
+
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
 
-	}
+System.out.println("Vamosa empezar a practicar");
+
+Perro Perro1 = new Perro("Yuma",4,12);
+
+System.out.println(Perro1.getNombre());
+System.out.println(Perro1.getEdad());
+System.out.println(Perro1.getPatas());
+
+Perro1.ladrar();
+System.out.println(suma(5));
+System.out.println(sumaI(7));
+}
+
+public static int suma(int A) {
+return A;
+}
+
+static int sumaI(int b) {
+return b;
+}
 
 }
