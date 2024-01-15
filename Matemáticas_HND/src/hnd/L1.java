02d89a214283e891072fdbb0aec53293fc8a2434
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-01-15 09:00:29 +0100

    new scanner

--- Matemáticas_HND/src/hnd/L1.java
+++ Matemáticas_HND/src/hnd/L1.java
@@ -1,5 +1,7 @@
 package hnd;
 
+import java.util.Scanner;
+
 public class L1 {
 
     public static int calcularMCD(int a, int b) {
@@ -11,12 +13,10 @@ public class L1 {
         return a;
     }
 
-    
     public static int calcularMCM(int a, int b) {
         return (a * b) / calcularMCD(a, b);
     }
 
-    
     public static int calcularMCDLista(int[] numeros) {
         int resultado = numeros[0];
         for (int i = 1; i < numeros.length; i++) {
@@ -25,7 +25,6 @@ public class L1 {
         return resultado;
     }
 
-    
     public static int calcularMCMLista(int[] numeros) {
         int resultado = numeros[0];
         for (int i = 1; i < numeros.length; i++) {
@@ -34,11 +33,20 @@ public class L1 {
         return resultado;
     }
 
-    
     public static void main(String[] args) {
-        // Ejemplo de uso
-        int[] numeros = {12, 18, 24};
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario la cantidad de números
+        System.out.print("Ingrese la cantidad de números: ");
+        int cantidadNumeros = scanner.nextInt();
+
+        // Pedir al usuario los números y almacenarlos en un array
+        int[] numeros = new int[cantidadNumeros];
+        for (int i = 0; i < cantidadNumeros; i++) {
+            System.out.print("Ingrese el número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
         // Calcular y mostrar el MCD
         int mcd = calcularMCDLista(numeros);
         System.out.println("El MCD de los números es: " + mcd);
@@ -46,8 +54,12 @@ public class L1 {
         // Calcular y mostrar el MCM
         int mcm = calcularMCMLista(numeros);
         System.out.println("El MCM de los números es: " + mcm);
+
+        // Cerrar el scanner
+        scanner.close();
+        
     }
-}
+    }
 
 
 
