dcfafad7d3fd48b2e794c274e6214d60b04a9d73
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-02-01 13:39:02 +0100

    get and set

--- POO/src/Ejercicio1/Animal.java
+++ POO/src/Ejercicio1/Animal.java
@@ -8,7 +8,29 @@ public class Animal {
 		LocalDate date;
 
 		public Animal(String nombre, LocalDate date) {
+			setNombre(nombre);
+			setDate(date);
+		}
+		
+		public Animal(String mote) {
+			setNombre(mote);
+			LocalDate hoyDate = LocalDate.of(2024, 02, 01);
+			setDate(hoyDate);
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
 			this.nombre = nombre;
+		}
+
+		public LocalDate getDate() {
+			return date;
+		}
+
+		public void setDate(LocalDate date) {
 			this.date = date;
 		}
 
