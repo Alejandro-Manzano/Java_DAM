ffe358501ea43bfb91c3a2e676d0c2053b886a20
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-02-02 12:49:27 +0100

    new object

---
+++ POO/src/ejercicio2/Hucha.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+public class Hucha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ POO/src/ejercicio2/principalHucha.java
@@ -0,0 +1,18 @@
+package ejercicio2;
+
+public class principalHucha {
+
+	boolean estaAbierta = true;
+	
+	String passwordString = "contraseña";
+	
+	int uno = 0;
+	int dos = 0;
+	int cinco = 0;
+	int diez = 0;
+	int veinte = 0;
+	int cincuenta = 0;
+	
+	int [] monedas = new int [2];
+	
+}
