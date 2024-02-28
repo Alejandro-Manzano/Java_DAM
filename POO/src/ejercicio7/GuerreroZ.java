713afd1f905b65e195bc31eb6cad4f1b8b05593c
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-02-28 11:51:18 +0100

    new space

---
+++ POO/src/ejercicio7/GuerreroZ.java
@@ -0,0 +1,26 @@
+package ejercicio7;
+
+public class GuerreroZ {
+	
+	String nombre;
+	boolean esSaiyan;
+	int vida, velocidad, ki, fuerza, defensa;
+	
+	public void atacar(Enemigo enemigo) {
+		
+	}
+	
+	public void kamehameHa(Enemigo enemigo) {
+		
+	}
+	
+	
+	public void transformarse() {
+		
+		if (esSaiyan == true) {
+			System.out.println("AAAAAAAAAAAAAAAAAAAAA");
+		}
+		
+	}
+
+}
